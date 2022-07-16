da390f8a9e292ec4865dfa72d39a991dafc0fe9d
@mithra <76215286+sreelekha-stack@users.noreply.github.com>
2022-07-16 05:00:54 +0000

    cahnges done

--- scraper.py
+++ scraper.py
@@ -1,1 +1,11 @@
-print("hello")
+import requests
+YOUTUBE_TRENDING_URL = "https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl"
+
+response = requests.get(YOUTUBE_TRENDING_URL)
+
+print('Status Code', response.status_code)
+print('Output', response.text[:1000])
+
+with open('trending.html', 'w') as f:
+  f.write(response.text)
+  
